038f25c34db43627c8501fb0ffa2ebcecb53ed2a
Deadpool2000 <32305505+Deadpool2000@users.noreply.github.com>
2020-03-20 21:59:28 +0530

    Update termuxyt.sh

--- termuxyt.sh
+++ termuxyt.sh
@@ -1,12 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo -e "Updating default packages\n"
+echo -e "Updating.....\n"
 apt update && apt upgrade -y
 
-echo -e "Installing packages\n"
+echo -e "Installing required packages.....\n"
 apt install wget python -y
 
-echo -e "Requesting access to storage\n"
+echo -e "Storage Permission.......\n"
 termux-setup-storage
 
 echo -e "Installing youtube-dl\n"
@@ -15,14 +15,15 @@ pip install youtube-dl
 echo -e "Creating Youtube folder\n"
 mkdir ~/storage/shared/youtube
 
-echo -e "Creating youtube-dl folder for config\n"
+echo -e "Creating youtube-dl configuration folder\n"
 mkdir -p ~/.config/youtube-dl
 
 echo -e "Creating bin folder\n"
 mkdir ~/bin
 
-echo -e "Downloading and installing termux-url-opener\n"
+echo -e "Installing termux-url-opener\n"
 wget https://pastebin.com/raw/L1QdBLiu -O ~/bin/termux-url-opener
+
 dos2unix ~/bin/termux-url-opener
 
 echo -e "\n"
